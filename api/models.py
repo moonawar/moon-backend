08664cfc6cd71a87cc0443d54d673fa7b5fa3bfc
moonawar <16521280@mahasiswa.itb.ac.id>
2022-01-13 03:44:34 +0700

    adding order field

--- api/models.py
+++ api/models.py
@@ -7,6 +7,7 @@ class Dialogue(models.Model):
     by = models.CharField(max_length=25)
     text = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+    order = models.PositiveSmallIntegerField(default=0, blank=True)
 
     def __str__(self):
         return f"Dialogue by {self.by} in {self.page}[{self.id}]"
