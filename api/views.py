9567d1640eadeab3c2cd2901717ea427a0936c02
moonawar <16521280@mahasiswa.itb.ac.id>
2022-01-12 22:02:36 +0700

    fixing deploy

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.db.models import query
 from django.http import request
 from django.shortcuts import render
-from rest_framework import generics, serializers
+from rest_framework import generics, serializers, permissions
 
 from api.models import Dialogue
 from api.serializers import DialogueSerializer
@@ -14,6 +14,7 @@ class CreateDialogueView(generics.CreateAPIView):
 class GetDialogueView(generics.ListAPIView):
     serializer_class = DialogueSerializer
     queryset = Dialogue.objects.all()
+    permissions_classes = [permissions.IsAuthenticated]
 
 class UpdateDialogueView(generics.UpdateAPIView):
     queryset = Dialogue.objects.all()
