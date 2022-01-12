a763ece99fd4a8c8adc66225b722da49780e08af
moonawar <16521280@mahasiswa.itb.ac.id>
2022-01-12 21:03:53 +0700

    fix wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -9,6 +9,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{project_name}}.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{backend}}.settings.production")
 
 application = get_wsgi_application()
